f7738be0e925ffbc0714146175e010ee50a3d64f
Khamid-S <solihovhamid@gmail.com>
2021-11-21 18:37:41 +0500

    some bugs fixed

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ signalCount = 0
 
 el = 0
 while el <len(data1)-20000:
-    el+=1000
+    el+=100
     j = 0
     sum = 0
     while(j<sampleRate1):
@@ -44,8 +44,8 @@ while el <len(data1)-20000:
     if (sum/sampleRate1>0.04):
         # print('element found')
         # print(el)
-        el+=sampleRate1
         signalPoints.append(el)
+        el+=sampleRate1
         signalCount+=1
 
 print(signalPoints)
@@ -56,13 +56,13 @@ fSignals = []
 
 
 for i in range(0, signalCount):
-    fSignals.append(np.fft.fft(data1[signalPoints[i]:signalPoints[i]+sampleRate1]))
-    plt.figure(i+2)
-    plt.title("signal " + str(i))
+    fSignals.append((np.fft.fft(data1[signalPoints[i]:signalPoints[i]+sampleRate1])))
+    plt.figure(i+3)
+    plt.title("signal " + str(i+1))
     plt.plot([abs(ele) for ele in fSignals[i]])
     print('--------------------------')
-    print('signal' + str(i))
-    for j in range(1, int(len(fSignals[i]) / 2) - 1):
+    print('signal' + str(i+1))
+    for j in range(1, int(len(fSignals[i]) / 2)):
         if fSignals[i][j] > fSignals[i][j - 1] and fSignals[i][j] > fSignals[i][j + 1] and fSignals[i][j] > 100:
             print(j)
 

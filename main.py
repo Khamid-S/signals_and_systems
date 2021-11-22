42418e23056464f6fe5ef49894d32bae1705e51f
Yakhyo Mashrapov <ymashrapov28@gmail.com>
2021-11-22 08:48:14 +0500

    few changes

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
-from playsound import playsound
-import winsound
+# from playsound import playsound
+# import winsound
 
-winsound.PlaySound('audio/Project3_v2.wav', winsound.SND_ASYNC) ## windows specific code
+# winsound.PlaySound('audio/Project3_v2.wav', winsound.SND_ASYNC) ## windows specific code
 
-data1, sampleRate1 = sf.read('audio/Project3_v2.wav')
+data1, sampleRate1 = sf.read('audio/Project3_vtest.wav')
 
 plt.figure(1)
 plt.title("Signal project 1")
 plt.plot([abs(ele) for ele in data1])
-
-
-
 
 result = np.fft.fft(data1)
 plt.figure(2)
@@ -21,8 +18,8 @@ plt.title("Signal F transform")
 
 
 for i in range(0, len(result)-10):
-    if abs(result[i])<200 and not(abs(result[i-10])>300 or abs(result[i+10])>300):
-        result[i]=0
+    if abs(result[i]) < 200 and not(abs(result[i-10]) > 300 or abs(result[i+10]) > 300):
+        result[i] = 0
 
 plt.plot([abs(ele) for ele in result])
 
@@ -34,19 +31,19 @@ signalPoints = []
 signalCount = 0
 
 el = 0
-while el <len(data1)-20000:
-    el+=100
+while el < len(data1)-20000:
+    el += 100
     j = 0
-    sum = 0
-    while(j<sampleRate1):
-        j+=1
-        sum += abs(cleanSignal[el])
-    if (sum/sampleRate1>0.04):
+    _sum = 0
+    while j < sampleRate1:
+        j += 1
+        _sum += abs(cleanSignal[el])
+    if _sum / sampleRate1 > 0.04:
         # print('element found')
         # print(el)
         signalPoints.append(el)
-        el+=sampleRate1
-        signalCount+=1
+        el += sampleRate1
+        signalCount += 1
 
 print(signalPoints)
 print(signalCount)
@@ -63,7 +60,8 @@ for i in range(0, signalCount):
     print('--------------------------')
     print('signal' + str(i+1))
     for j in range(1, int(len(fSignals[i]) / 2)):
-        if abs(fSignals[i][j]) > abs(fSignals[i][j - 1]) and abs(fSignals[i][j]) > abs(fSignals[i][j + 1]) and abs(fSignals[i][j]) > 100:
+        if abs(fSignals[i][j]) > abs(fSignals[i][j - 1]) and \
+                abs(fSignals[i][j]) > abs(fSignals[i][j + 1]) and abs(fSignals[i][j]) > 100:
             print(j)
 
 plt.show()
